fcedf04ee14db4c4bc63ff3a783715931b7e066a
Mariela Enciso <35668707+DJMare@users.noreply.github.com>
2019-10-29 19:19:58 -0700

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+//Add require mysql
+const mysql = require('mysql');
+
+//Update to connect to the MySQL database
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'sakila'
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Yay! You are connected to the database!');
+})
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
